5a2d5d9f031efe219911cc2fdc73f9cd35896a56
TROCADER <60465392+TROCADER@users.noreply.github.com>
2020-01-30 14:53:18 +0100

    Lade till vinstschenarios

--- js/script.js
+++ js/script.js
@@ -10,7 +10,48 @@ function getWinner()
     let square8 = document.querySelector("square8");
     let square9 = document.querySelector("square9");
 
+    //Vinstschenario vågrätt 1
     if (square1.innerHTML !== "" && square1.innerHTML === square2.innerHTML && square1.innerHTML === square3.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    //Vinstschenario vågrätt 2
+    else if (square4.innerHTML !== "" && square4.innerHTML === square5.innerHTML && square4.innerHTML === square6.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    //Vinstschenario vågrätt 2
+    else if (square7.innerHTML !== "" && square7.innerHTML === square8.innerHTML && square4.innerHTML === square9.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    //Vinstschenario lodrätt 1
+    else if (square1.innerHTML !== "" && square1.innerHTML === square4.innerHTML && square1.innerHTML === square7.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    //Vinstschenario lodrätt 2
+    else if (square2.innerHTML !== "" && square2.innerHTML === square5.innerHTML && square2.innerHTML === square8.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    //Vinstschenario lodrätt 3
+    else if (square3.innerHTML !== "" && square3.innerHTML === square6.innerHTML && square3.innerHTML === square9.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    else if (square1.innerHTML !== "" && square1.innerHTML === square5.innerHTML && square1.innerHTML === square9.innerHTML)
+    {
+        console.log("You won");
+    }
+
+    else if (square3.innerHTML !== "" && square3.innerHTML === square5.innerHTML && square3.innerHTML === square7.innerHTML)
     {
         console.log("You won");
     }
